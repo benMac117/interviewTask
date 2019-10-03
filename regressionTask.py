3ba775ef1ab2549d3eae79ed7b18fcf24edd71ac
ben <ben.mac117@outlook.com>
2019-10-03 16:53:43 +0100

    managing simple train/test split

--- regressionTask.py
+++ regressionTask.py
@@ -19,10 +19,23 @@ class LinearRegressor:
             gradients = self.calculate_gradient
 
 dataFilepath = '/home/ben/Data/morsum/avocadoCleaned.csv'
-data = np.loadtxt(dataFilepath, delimiter=',', skiprows=1)
-targets = data[:,-1]
-data = data[:,:-1]
-print(targets.shape)
-print(data.shape)
-print(targets[0])
-print(data[0])
+features = np.loadtxt(dataFilepath, delimiter=',', skiprows=1)
+targets = features[:,-1]
+features = features[:,:-1]
+
+# Take a random 10% of the dataset for testing
+numSamples = len(targets)
+testingIndices = np.random.choice(range(numSamples), int(numSamples/10), replace=False)
+trainingIndices = np.setdiff1d(range(numSamples), testingIndices)
+
+trainingFeatues = features[trainingIndices]
+trainingTargets = targets[trainingIndices]
+testingFeatures = features[testingIndices]
+testingTargets = targets[testingIndices]
+
+regr = LinearRegressor()
+regr.fit(trainingFeatues, trainingTargets)
+
+predictions = regr.predict(testingFeatures)
+
+# stretch to implement cross-validation
