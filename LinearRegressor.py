9c22382b6c5a07bc22ec7245974d6157b66d7790
ben <ben.mac117@outlook.com>
2019-10-03 19:05:03 +0100

    renaming variables for better clarity

--- LinearRegressor.py
+++ LinearRegressor.py
@@ -6,17 +6,17 @@ class LinearRegressor:
         self.epochCap = epochCap # How may training iterations the model should take
 
     # I'm including the weights as a parameter rather than using self.weights to make testing easier
-    def predict(self, X, weights):
+    def predict(self, samples, weights):
         """Make a prediction for a given input"""
-        return np.dot(X, weights) # take the dot product of input with weights
+        return np.dot(X, samples) # take the dot product of input with weights
 
-    def cost(self, yPred, yTrue):
+    def cost(self, predictions, targets):
         """Calculate the current cost/loss for the model"""
-        return np.mean(abs(yTrue - yPred))
+        return np.mean(abs(targets - predictions))
 
-    def calculate_gradients(self, X, yPred, y):
+    def calculate_gradients(self, samples, predictions, targets):
         """Calculate the gradients for optimising the model parameters"""
-        return np.dot(X.T, (yPred - y)) / len(X)
+        return np.dot(samples.T, (predictions - targets)) / len(targets)
 
     # This return value is purely to make testing more clean, happy to discuss whether it is correct
     # Same with using weights rather than just self.weights -= ...
@@ -24,11 +24,11 @@ class LinearRegressor:
         self.weights = weights - lRate * gradients
         return self.weights 
 
-    def fit(self, X, y):
-        self.weights = np.zeros(X.shape[1])
+    def fit(self, samples, targets):
+        self.weights = np.zeros(samples.shape[1])
 
         for epochCount in range(self.epochCap):
-            yPred = self.predict(X)
+            predictions = self.predict(samples)
 
-            gradients = self.calculate_gradient(self.weights)
-            self.apply_gradients(gradients, self.weights)
+            gradients = self.calculate_gradient(samples, predictions, targets)
+            self.apply_gradients(self.weights, self.lRate, gradients)
