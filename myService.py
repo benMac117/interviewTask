618f7ac9a0a83d4e7ea3f9c628a1206dd8977e4f
ben <ben.mac117@outlook.com>
2019-10-04 14:29:48 +0100

    implementing the many-to-many relationship between tables

--- myService.py
+++ myService.py
@@ -24,8 +24,8 @@ ma = Marshmallow(app)
 
 # Create a table of links to hold the mabny-to-many relationship between recipes and ingredients
 used = db.Table('used',
-	db.Column('recipeID', Integer, db.ForeignKey('Recipe.id')),
-	db.Column('ingredientID', Integer, db.ForeignKey('Ingredient.id')),
+    db.Column('recipeID', Integer, db.ForeignKey('Recipe.id')),
+    db.Column('ingredientID', Integer, db.ForeignKey('Ingredient.id')),
 )
 
 class Recipe(db.Model):
@@ -52,10 +52,12 @@ class Ingredient(db.Model):
 
 # model schemas
 class RecipeSchema(ma.Schema):
+    ingredients = ma.Nested('IngredientSchema', many=True, exclude=("recipes",))
     class Meta:
         fields = ('id', 'name', 'instructions', 'ingredients')
 
 class IngredientSchema(ma.Schema):
+    recipes = ma.Nested(RecipeSchema, many=True)
     class Meta:
         fields = ('id', 'name', 'recipes')
 
@@ -66,65 +68,88 @@ ingredientSchema = IngredientSchema(strict=True)
 ingredientsSchema = IngredientSchema(strict=True, many=True)
 
 def commit_to_db(db):
-	try:
-		db.session.commit()
-	except Exception as e:
-		print('An error occured when committing change to the database')
-		print(e)
+    try:
+        db.session.commit()
+    except Exception as e:
+        print('An error occured when committing change to the database')
+        print(e)
 
 # Recipe routing
 # Create a recipe
 @app.route('/recipes', methods=['POST'])
 def add_recipe():
-	newRecipe = Recipe(request.json['name'], request.json['instructions'])
-	db.session.add(newRecipe)
-	commit_to_db(db)
-	return recipeSchema.jsonify(newRecipe), 201
+    newRecipe = Recipe(request.json['name'], request.json['instructions'])
+    db.session.add(newRecipe)
+    commit_to_db(db)
+    return recipeSchema.jsonify(newRecipe), 201
 
 # Get all recipes
 @app.route('/recipes', methods=['GET'])
-def get_products():
-	allRecipes = Recipe.query.all()
-	result = recipesSchema.dump(allRecipes)
-	return jsonify(result.data)
+def get_recipes():
+    allRecipes = Recipe.query.all()
+    result = recipesSchema.dump(allRecipes)
+    return jsonify(result.data)
+
+# Get single recipe
+@app.route('/recipes/<id>', methods=['GET'])
+def get_recipe(id):
+    recipe = Recipe.query.get(id)
+    return recipeSchema.jsonify(recipe)
+
+@app.route('/recipes/<id>', methods=['PUT'])
+def update_recipes(id):
+    recipe = Recipe.query.get(id)
+
+    recipe.name = request.json['name']
+    recipe.instructions = request.json['instructions']
+    ingredientIDs = request.json['ingredientIDs']
+    #from IPython import embed; embed()
+    recipe.ingredients = [Ingredient.query.get(ingID) for ingID in ingredientIDs]
+
+    db.session.commit()
+
+    return recipeSchema.jsonify(recipe)
+
+# Ingredient routing
+@app.route('/ingredients', methods=['POST'])
+def add_ingredient():
+    newIngredient = Ingredient(request.json['name'])
+    db.session.add(newIngredient)
+    commit_to_db(db)
+    print(newIngredient)
+    return ingredientSchema.jsonify(newIngredient), 201
+
+# Get all recipes
+@app.route('/ingredients', methods=['GET'])
+def get_ingredients():
+    allIngredients = Ingredient.query.all()
+    result = ingredientsSchema.dump(allIngredients)
+    return jsonify(result.data)
 '''
-# Get all products
-@app.route('/product', methods=['GET'])
-def get_products():
-	allProducts = Product.query.all()
-	result = products_schema.dump(allProducts)
-	return jsonify(result.data)
-
-# Get single product
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-	product = Product.query.get(id)
-	return product_schema.jsonify(product)
-
 # Update a product
 @app.route('/product/<id>', methods=['PUT'])
 def update_product(id):
-	product = Product.query.get(id)
+    product = Product.query.get(id)
 
-	product.name = request.json['name']
-	product.description = request.json['description']
-	product.price = request.json['price']
-	product.qty = request.json['qty']
+    product.name = request.json['name']
+    product.description = request.json['description']
+    product.price = request.json['price']
+    product.qty = request.json['qty']
 
-	db.session.commit()
+    db.session.commit()
 
-	return product_schema.jsonify(product)
+    return product_schema.jsonify(product)
 
 # Delete product
 @app.route('/product/<id>', methods=['DELETE'])
 def delete_product(id):
-	product = Product.query.get(id)
-	db.session.delete(product)
-	db.session.commit()
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
 
-	return product_schema.jsonify(product), 202
+    return product_schema.jsonify(product), 202
 
 '''
 if __name__ == '__main__':
-	# debug=True should only be used during development
+    # debug=True should only be used during development
     app.run(debug=True)
